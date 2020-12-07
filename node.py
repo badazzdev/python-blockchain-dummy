53591b5984f5eb70699a03203ce644e768f7dc5f
Axel <axel.adrian@me.com>
2020-12-07 19:55:56 +0100

    Enable flask debug mode

--- node.py
+++ node.py
@@ -282,4 +282,4 @@ if __name__ == '__main__':
     port = args.port
     wallet = Wallet(port)
     blockchain = Blockchain(wallet.public_key, port)
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
